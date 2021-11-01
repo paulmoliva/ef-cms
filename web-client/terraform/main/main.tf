1ed7f7feb26cb6b3285ba24aac7f21113faa2522
Michael Marcotte <mike.marcotte@gmail.com>
2021-11-01 09:49:36 -0400

    add alarm for api health check

--- web-client/terraform/main/main.tf
+++ web-client/terraform/main/main.tf
@@ -239,6 +239,24 @@ resource "aws_route53_health_check" "status_health_check" {
   regions            = ["us-east-1", "us-west-1", "us-west-2"] # Minimum of three regions required
 }
 
+resource "aws_cloudwatch_metric_alarm" "api_health_check" {
+  alarm_name          = "api.${var.dns_domain} health check endpoint"
+  namespace           = "AWS/Route53"
+  metric_name         = "HealthCheckStatus"
+  comparison_operator = "LessThanThreshold"
+  statistic           = "Minimum"
+  threshold           = "1"
+  evaluation_periods  = "2"
+  period              = "60"
+
+  dimensions = {
+    HealthCheckId = aws_route53_health_check.api_health_check.id
+  }
+
+  alarm_actions             = [data.aws_sns_topic.system_health_alarms.arn]
+  insufficient_data_actions = [data.aws_sns_topic.system_health_alarms.arn]
+  ok_actions                = [data.aws_sns_topic.system_health_alarms.arn]
+}
 
 resource "aws_route53_health_check" "api_health_check" {
   fqdn               = "api.${var.dns_domain}"
